4cebac74ac6e17f97e58dbbc99190606f32cb943
Gabriel Molezini <FMG010@einstein.br>
2023-06-19 11:02:58 -0300

    .

--- src/data/protocols/db/patients/createPatientRepository.ts
+++ src/data/protocols/db/patients/createPatientRepository.ts
@@ -3,6 +3,6 @@ import { CreatePatientInterface } from '@/domain/usecases/patient'
 export interface CreatePatientRepository extends CreatePatientInterface {}
 
 export namespace CreatePatientRepository {
-  export type Request = CreatePatientInterface.Request
+  export type Params = CreatePatientInterface.Params
   export type Result = CreatePatientInterface.Result
 }

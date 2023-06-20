02bb7e8bf9854ec88427be4ed17016d05f5b25b1
Gabriel Molezini <FMG010@einstein.br>
2023-06-20 14:12:18 -0300

    test: delete patient controller

---
+++ tests/presentation/controllers/patients/deletePatientController.spec.ts
@@ -0,0 +1,31 @@
+import { makeDeletePatientRepository } from '@/main/factories/patients/delete/deletePatientUseCaseFactory'
+import { DeletePatientController } from '@/presentation/controllers'
+
+interface SutTypes {
+  sut: DeletePatientController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new DeletePatientController(makeDeletePatientRepository())
+
+  return {
+    sut,
+  }
+}
+
+describe('CreatePatientController', () => {
+  test('should return 404 if patient no exist', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      params: {
+        id: 1,
+      },
+    }
+
+    await expect(sut.handle(httpRequest)).resolves.toEqual({
+      statusCode: 400,
+      body: 'Patient does not exist',
+    })
+  })
+})

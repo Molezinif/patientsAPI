3fc1652219c85fa5e057826bf58486099d0990f4
Gabriel Molezini <FMG010@einstein.br>
2023-06-21 13:43:13 -0300

    hotfix: findPatientsRepo params

--- src/data/protocols/db/patients/findPatientsRepository.ts
+++ src/data/protocols/db/patients/findPatientsRepository.ts
@@ -3,6 +3,5 @@ import { FindPatientsInterface } from '@/domain/usecases/patient'
 export interface FindPatientsRepository extends FindPatientsInterface {}
 
 export namespace FindPatientsRepository {
-  export type Params = FindPatientsInterface.Params
   export type Result = FindPatientsInterface.Result
 }
